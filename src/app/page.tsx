1ed7070141db4ebd39f0333b40b16cfd92d13b2a
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2025-07-15 13:34:06 +0300

    auth & db sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+import prisma from "@/lib/prisma";
+
+export default async function Home() {
 	return (
 		<div className="m-4">
 			<h1>Home Page Content</h1>
